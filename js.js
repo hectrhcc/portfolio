9039e4eec8b6588b8c3b01abdadf87fec14efc4b
Hector <hcontrerascorvacho@gmail.com>
2024-06-26 17:56:11 -0400

    design

--- js.js
+++ js.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
     // Cargar contenido inicial (puedes ajustar esto según tu necesidad)
     $('#contenido-dinamico').load('inicio.html');
-
+    $(this).addClass('loaded'); 
     // Manejar clicks en los enlaces
     $('.cargar-contenido').click(function(e) {
         e.preventDefault(); // Evitar comportamiento por defecto del enlace
 
         var pagina = $(this).attr('href'); // Obtener el enlace
 
-        // Cargar la página usando AJAX
-        $('#contenido-dinamico').load(pagina);
+        $('#contenido-dinamico').removeClass('loaded');
 
-        // Opcional: cambiar la URL en el navegador sin recargar la página completa
-        history.pushState(null, null, pagina);
+        // Cargar la página usando AJAX
+        $('#contenido-dinamico').load(pagina, function() {
+            $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el nuevo contenido
+        });
 
         // Retornar false para evitar que el navegador siga el enlace
         return false;
