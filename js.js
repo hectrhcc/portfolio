be3e017e1b600d55220624188d7a9b76454b968a
Hector <hcontrerascorvacho@gmail.com>
2024-06-26 23:07:58 -0400

    hermosa

--- js.js
+++ js.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
+    $('#texto').typeIt({
+            speed: 50
+     });
     // Cargar contenido inicial
     $('#contenido-dinamico').load('inicio.html', function() {
         $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el contenido inicial
     });
-
     // Manejar clicks en los enlaces
     $('.cargar-contenido').click(function(e) {
         e.preventDefault(); // Evitar comportamiento por defecto del enlace
-
-        var pagina = $(this).attr('href'); // Obtener el enlace
-
+        let pagina = $(this).attr('href'); // Obtener el enlace
         $('#contenido-dinamico').removeClass('loaded');
-
         // Cargar la página usando AJAX
         $('#contenido-dinamico').load(pagina, function() {
             $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el nuevo contenido
         });
-
         // Retornar false para evitar que el navegador siga el enlace
         return false;
     });
