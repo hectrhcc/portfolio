d6e79a5813aa5f151a5e539bbee92c038c74cc4c
Hector <hcontrerascorvacho@gmail.com>
2024-06-27 17:47:55 -0400

    js full

---
+++ js.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // Cargar contenido inicial
+    $('#contenido-dinamico').load('inicio0.html', function() {
+        $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el contenido inicial
+    });
+    // Manejar clicks en los enlaces
+    $('.cargar-contenido').click(function(e) {
+        e.preventDefault(); // Evitar comportamiento por defecto del enlace
+        let pagina = $(this).attr('href'); // Obtener el enlace
+        $('#contenido-dinamico').removeClass('loaded');
+        // Cargar la página usando AJAX
+        $('#contenido-dinamico').load(pagina, function() {
+            $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el nuevo contenido
+        });
+        // Retornar false para evitar que el navegador siga el enlace
+        return false;
+    });
+    $('#texto').typeIt({
+        speed: 50
+ });
+});
