29df8a1ce2b5164d05e96f2af0b42931c0ca2a37
Hector <hcontrerascorvacho@gmail.com>
2024-08-02 17:49:40 -0400

    sin margin-rigth

--- js.js
+++ js.js
@@ -3,14 +3,15 @@ $(document).ready(function() {
     $('#contenido-dinamico').load('inicio.html', function() {
         $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el contenido inicial
     });
-    // Manejar clicks en los enlaces
+
     $('.cargar-contenido').click(function(e) {
         e.preventDefault(); // Evitar comportamiento por defecto del enlace
         let pagina = $(this).attr('href'); // Obtener el enlace
-        $('#contenido-dinamico').removeClass('loaded');
+
+        $('#contenido-dinamico').removeClass('loaded').addClass('loading'); 
         // Cargar la página usando AJAX
         $('#contenido-dinamico').load(pagina, function() {
-            $(this).addClass('loaded'); // Aplicar clase loaded después de cargar el nuevo contenido
+            $(this).removeClass('loading').addClass('loaded'); // Aplicar clase loaded después de cargar el nuevo contenido
         });
         // Retornar false para evitar que el navegador siga el enlace
         return false;
